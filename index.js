0455c2cf7c7b1d9307695040051812a16ec9ab4b
naveenyeluru <79347548+naveenyeluru@users.noreply.github.com>
2025-05-09 07:05:33 +0400

    updated index

--- index.js
+++ index.js
@@ -24,10 +24,20 @@ const client = new Client({
     dataPath: './session',
   }),
   puppeteer: {
-    headless: false, // <-- Add this
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  }
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--no-zygote',
+      '--single-process',
+      '--disable-software-rasterizer'
+    ],
+  },
 });
+
 
 
 // QR Code Generation
